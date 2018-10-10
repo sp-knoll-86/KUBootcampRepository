fbf824fe37474e3d7f42a3355ede5b3f7b76eaad
Shaun Befort <sbefort@gmail.com>
2018-10-09 20:01:58 -0500

    Best things ever solution

---
+++ 01-Class-Content/10-nodejs/01-Activities/13-BestThingsEver/Solved/readBest.js
@@ -0,0 +1,20 @@
+// Includes the FS package for reading and writing packages
+var fs = require("fs");
+
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable "data"
+fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // Break the string down by comma separation and store the contents into the output array.
+  var output = data.split(",");
+
+  // Loop Through the newly created output array
+  for (var i = 0; i < output.length; i++) {
+
+    // Print each element (item) of the array/
+    console.log(output[i]);
+  }
+});
