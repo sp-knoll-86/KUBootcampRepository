a0e1194612e031f04e0790ebf4d60c7f86eb912c
Shaun Befort <sbefort@gmail.com>
2018-10-11 21:04:44 -0500

    Inquirer Geocode solution

---
+++ 01-Class-Content/10-nodejs/01-Activities/27-Inquirer_Geocode/Solved/inquirerGeocode.js
@@ -0,0 +1,43 @@
+// Instructions:
+// Convert the below geocoding code from earlier today so that it uses inquirer.js instead of process.argv to handle the intake of user inputs.
+// Make sure your new code provides the exact same output as it did with process.argv.
+
+// HINT: Don't forget to create your package.json file and save the correct packages to it.
+// HINT: You should not need to change *that much* code.
+
+// ========================================================================================================================
+
+// Include the node-geocoder and inquirer NPM packages
+var NodeGeocoder = require("node-geocoder");
+var inquirer = require("inquirer");
+
+// Replace with your mapquest consumer API key
+var options = {
+  provider: "mapquest",
+  apiKey: "YOUR-MAPQUEST-API-CONSUMER-KEY"
+};
+
+var geocoder = NodeGeocoder(options);
+
+
+// Prompt the user to provide location information.
+inquirer.prompt([
+
+  {
+    type: "input",
+    name: "userInput",
+    message: "Which location or landmark would you like to geocode?"
+  }
+
+// After the prompt, store the user's response in a variable called location.
+]).then(function(location) {
+
+  // console.log(location.userInput);
+
+  // Then use the Google Geocoder to Geocode the address
+  geocoder.geocode(location.userInput, function(err, data) {
+
+    console.log(JSON.stringify(data, null, 2));
+  });
+
+});
