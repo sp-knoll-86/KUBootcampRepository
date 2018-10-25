91fbcdf9c82b19ef6892cd7a54dd96cf80898488
Shaun Befort <sbefort@gmail.com>
2018-10-24 20:07:51 -0500

    MySQL Solutions

---
+++ 01-Class-Content/12-mysql/01-Activities/08-playlistRead/Solved/playlistRead.js
@@ -0,0 +1,42 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "playlistDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllSongs();
+  queryDanceSongs();
+});
+
+function queryAllSongs() {
+  connection.query("SELECT * FROM songs", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+    }
+    console.log("-----------------------------------");
+  });
+}
+
+function queryDanceSongs() {
+  var query = connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+    }
+  });
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
