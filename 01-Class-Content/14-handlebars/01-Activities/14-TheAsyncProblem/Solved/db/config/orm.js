928de42c9ae0ec3d4773bdee0f19797eeee339ba
Shaun Befort <sbefort@gmail.com>
2018-11-08 20:44:41 -0600

    async problem solution

---
+++ 01-Class-Content/14-handlebars/01-Activities/14-TheAsyncProblem/Solved/db/config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  // The last variable cb represents the anonymous function being passed from server.js
+  selectWhere: function(tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
