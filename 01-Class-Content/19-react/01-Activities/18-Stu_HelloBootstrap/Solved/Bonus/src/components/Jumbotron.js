7d425238418407ac40077962ba4593b2ed880571
Shaun Befort <sbefort@gmail.com>
2018-12-15 12:23:08 -0600

    Hello Bootstrap Solution

---
+++ 01-Class-Content/19-react/01-Activities/18-Stu_HelloBootstrap/Solved/Bonus/src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1>Your Project</h1>
+      <p>
+        Enim adipisicing enim reprehenderit ex ullamco consectetur Lorem laboris magna exercitation.
+        Aliquip reprehenderit magna elit cillum adipisicing dolore aliquip velit. Ipsum ullamco
+        nostrud tempor eu deserunt ipsum incididunt fugiat esse ipsum. Exercitation nostrud
+        exercitation sit ex nostrud aliqua officia magna nostrud deserunt et esse eu deserunt. Non
+        dolor consequat qui ea culpa tempor magna nulla consectetur est occaecat.
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;
