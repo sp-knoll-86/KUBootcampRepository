39b6009152a8f5696d91440dd2b97abc33ff3369
Shaun Befort <sbefort@gmail.com>
2018-12-15 11:42:09 -0600

    React Hello Div Solution

---
+++ 01-Class-Content/19-react/01-Activities/16-Stu_HelloDiv/Solved/src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+  return (
+    <div>
+      <h1>Hey guys! I'm a header!</h1>
+      <h2>Things I like!</h2>
+      <ul>
+        <li>Peas</li>
+        <li>Carrots</li>
+        <li>Love</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
