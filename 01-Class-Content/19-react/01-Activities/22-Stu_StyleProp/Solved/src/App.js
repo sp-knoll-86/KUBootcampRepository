6abaac3225e4ca84e629d360975895bb4b34cd92
Shaun Befort <sbefort@gmail.com>
2018-12-15 13:22:38 -0600

    React Inline Styles Solution

---
+++ 01-Class-Content/19-react/01-Activities/22-Stu_StyleProp/Solved/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
