87f2c35bb2362f3178ae45941d05f33539c0730f
Shaun Befort <sbefort@gmail.com>
2018-12-20 19:52:50 -0600

    Conditional Render Solution

---
+++ 01-Class-Content/20-react/01-Activities/04-Stu_ConditionalRender/Solved/src/components/PortfolioContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+class Portfolio extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "About") {
+      return <About />;
+    } else if (this.state.currentPage === "Blog") {
+      return <Blog />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
